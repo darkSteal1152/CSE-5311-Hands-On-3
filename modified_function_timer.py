51e8be0aa7de7a17d69b1702a171b25613df7dc2
darkSteal1152 <89552151+darkSteal1152@users.noreply.github.com>
2024-09-06 21:21:50 -0500

    Create modified_function_timer.py

---
+++ modified_function_timer.py
@@ -0,0 +1,37 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(n):
+    x = 1
+    y = 1
+    for i in range(1, n):
+        for j in range(1, n):
+            x = x + 1
+            y = i + j
+
+    return x
+
+input_values = np.concatenate([np.arange(1, 10), np.arange(10, 100, 5), np.arange(100, 1000, 50), np.arange(1000, 10000, 500)])
+times = []
+
+for n in  input_values:
+    start_time = time.time()
+    f(n)
+    times.append(time.time() - start_time)
+
+polynomial_coefficients = np.polyfit(input_values, times, 2)
+polynomial = np.poly1d(polynomial_coefficients)
+
+fitted_times = polynomial(input_values)
+
+plt.plot(input_values, times, label="Observed Modified f(n)")
+plt.plot(input_values, fitted_times, label="Estimated Modified f(n)", linestyle="--")
+plt.xlabel('n')
+plt.ylabel('Time (seconds)')
+plt.title('Function Benchmark')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+print(polynomial)
